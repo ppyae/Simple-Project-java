130e9088bf50e939bc851abb4f18a0aaf9dec084
phyo <moehtat027@gmail.com>
2024-11-04 03:28:56 +0700

    Capitalize

--- Simple project/CapitalLetter.java
+++ Simple project/CapitalLetter.java
@@ -3,8 +3,8 @@ public class CapitalLetter{
 	public static String change(String str){
 
 		 return Arrays.stream(str.split("\\s"))
-                     .map(word -> Character.toTitleCase(word.charAt(0)) + word.substring(1))
-                     .collect(Collectors.joining(" "));
+		 				.map(message -> Character.toUpperCase(message.charAt(0))+ message.substring(1))
+		 				.collect(Collectors.joining(" "));
     }
 
 }
